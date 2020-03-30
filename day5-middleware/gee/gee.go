ea094b02cf2b13f11a1ec7b9253af359f48ca761
fanxiao <1271410507@qq.com>
2020-03-30 22:56:44 +0800

    day4-group day5-middleware

---
+++ day5-middleware/gee/gee.go
@@ -0,0 +1,97 @@
+/*
+
+@Time : 2020/3/17 11:32 AM
+*/
+package gee
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+// 定义 request handler
+type HandlerFunc func(*Context)
+
+// 定义Engine 实现ServeHTTP接口
+// 作为最顶层分组
+type Engine struct {
+	*RouterGroup
+	router *router        // 路由表
+	groups []*RouterGroup //
+}
+
+type RouterGroup struct {
+	prefix      string        // 分组路由前缀
+	middlewares []HandlerFunc // 中间件
+	engine      *Engine       // engine
+}
+
+// Group创建RouterGroup实例 所有group共享同一个engine
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+// 添加中间件
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
+func (group *RouterGroup) addRouter(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRouter("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRouter("POST", pattern, handler)
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range e.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := newContext(w, req)
+	c.handlers = middlewares
+	e.router.handle(c)
+}
+
+func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	e.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (e *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, e)
+}
